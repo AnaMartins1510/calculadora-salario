79fd3bca371d66349e45c3e5d4efa6ccfefb5577
Home <home@MacBook-Pro-de-Home.local>
2022-08-28 13:31:59 -0300

    atualizando a calculadora

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+let label = document.getElementsByTagName('label')[0];
+let input = document.getElementsByTagName('input')[0];
+
+label.onclick =function (){
+    label.style.display = "none";
+    input.focus();
+};
+input.onblur = function(){
+    if(this.value) label.style.display = 'block';
+};
+
+let label2 = document.getElementsByTagName('label')[1];
+let input = document.getElementsByTagName('input')[1];
+
+label2.onclick = function (){
+    label2.style.display = "none";
+    input.focus();
+
+};
+input2.onblur = function(){
+    if(this.value) label2.style.display = 'block';
+
+};
+const horasDiarias = 8
+
 const calcularSalario = () => {
     const diasTrabalhados = document.querySelector('#dias').value
     const valorHora = documente.querySelector ('#salario_hora').value
-    const valorDia = valorHora * 8 
+    const valorDia = valorHora * horasDiarias 
     const salarioMes = valorDia * diasTrabalhados
-    const salarioTotal = document.querySelector('#salarioBruto')
+    const salarioTotal = document.querySelector('#salario_total')
     salarioTotal.innerHTML = 'R$' + salarioMes.toFixed(2)
 }
 
